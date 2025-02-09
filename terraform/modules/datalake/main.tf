8da45bb7ca885f901277321ff4b622d529c59b50
Krijn van der Burg <krijnvanderburg@protonmail.com>
2025-02-09 11:03:58 +0100

    test

--- terraform/modules/datalake/main.tf
+++ terraform/modules/datalake/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_account" "source" {
-  name                     = "st${var.storage_account_name}${var.environment}${var.location_primary_abbr}${var.sequence_number}"
+  name                     = "st${var.source_storage_account_name}${var.environment}${var.source_location_primary_abbr}${var.sequence_number}"
   resource_group_name      = var.resource_group_name
-  location                 = var.location_primary
+  location                 = var.source_location_primary
   account_tier             = "Standard"
-  account_replication_type = var.account_replication_type
+  account_replication_type = var.source_account_replication_type
 
   lifecycle {
     prevent_destroy = true
@@ -19,7 +19,7 @@ resource "azurerm_management_lock" "source_lock" {
 resource "azurerm_storage_container" "container" {
   for_each              = var.containers
   name                  = each.value
-  storage_account_id    = azurerm_storage_account.source.id
+  storage_account_id    = azurerm_storage_account.source.source.id
   container_access_type = "private"
 
   lifecycle {
