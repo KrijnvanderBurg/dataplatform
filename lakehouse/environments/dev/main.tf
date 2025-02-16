35fa61e944ab9acd2566390561ef0b66228f85ee
Krijn van der Burg <krijnvanderburg@protonmail.com>
2025-02-16 21:26:24 +0000

    test

--- lakehouse/environments/dev/main.tf
+++ lakehouse/environments/dev/main.tf
@@ -1,25 +1,3 @@
-/**
- * Azure Databricks workspace in custom VNet
- *
- * Module creates:
- * * Resource group with random prefix
- * * Tags, including `Owner`, which is taken from `az account show --query user`
- * * VNet with public and private subnet
- * * Databricks workspace
- */
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.104.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
-
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id = var.tenant_id
@@ -28,7 +6,7 @@ provider "azurerm" {
 }
 
 module "adb_with_private_links_exfiltration_protection" {
-  source           = "./modules/adb-with-private-links-exfiltration-protection"
+  source           = "./modules/lakehouse"
   vnet_hub_cidr          = var.vnet_hub_cidr
   vnet_spoke_cidr        = var.vnet_spoke_cidr
   location       = var.location
